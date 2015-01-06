a08f5bbbab42f29a7d4a96458f5939d4d2af7d9c
0rigen <cparson2@gmail.com>
2015-01-06 11:06:51 -0500

    initial stuff

--- src/Pandora.py
+++ src/Pandora.py
@@ -8,8 +8,16 @@ print """\
 """
 print "Let's get funky\n\n"
 
+def main_menu
 print "(S)imple Crypto Checks"
 print "(A)dvanced Operations"
 print "(P)unch 1o57 in the face"
 
 choice = raw_input(">>> ")
+
+if choice in ('S','s'):
+ print 'You chose Simple'
+if choice in ('A','a'):
+ print 'You chose Advanced'
+if choice in ('P','p'):
+ print 'You punched 1057!  I hope you feel a little better now...'
