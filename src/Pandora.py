f46389d91e93f538f3ae54a3cdac33a647facabb
0rigen <cparson2@gmail.com>
2015-01-06 11:33:21 -0500

    initial stuff

--- src/Pandora.py
+++ src/Pandora.py
@@ -1,6 +1,8 @@
 __author__ = 'Origen'
 __project__ = 'Pandora'
 
+import sys
+
 print """\
  __             __   __   __
 |__)  /\  |\ | |  \ /  \ |__)  /\\
@@ -8,16 +10,25 @@ print """\
 """
 print "Let's get funky\n\n"
 
-def main_menu
-print "(S)imple Crypto Checks"
-print "(A)dvanced Operations"
-print "(P)unch 1o57 in the face"
+class Menu:
+ def Showmenu(self):
+  print "(S)imple Crypto Checks"
+  print "(A)dvanced Operations"
+  print "(P)unch 1o57 in the face"
+  print "(Q)uit Pandora"
 
-choice = raw_input(">>> ")
+ def getChoice(self):
+  choice = raw_input(">>> ")
+  if choice in ('S','s'):
+   print 'You chose Simple.'
+  if choice in ('A','a'):
+   print 'You chose Advanced'
+  if choice in ('P','p'):
+   print 'You punched 1057!  I hope you feel a little better now...'
+  if choice in ('Q','q'):
+   print 'Good luck!'
+   sys.exit(0)
 
-if choice in ('S','s'):
- print 'You chose Simple'
-if choice in ('A','a'):
- print 'You chose Advanced'
-if choice in ('P','p'):
- print 'You punched 1057!  I hope you feel a little better now...'
+m = Menu()
+m.Showmenu()
+m.getChoice()
