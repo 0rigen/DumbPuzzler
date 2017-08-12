ec69a6b373d69b88c5531523a69e084b8e9efa7a
0rigen <cparson2@gmail.com>
2017-08-11 21:25:44 -0400

    Restructured encoding checks

--- src/DumbPuzzler.py
+++ src/DumbPuzzler.py
@@ -1,9 +1,14 @@
-#!/usr/bin/env/ python
+#!/usr/bin/env python3
 """ Provides a number of crypto checks to try and quickly decipher simple crypto
 
 Takes a string of ciphertext and a chunk size, then attempts to decipher
 each chunk according to a series of known cipher methods.  All outputs
 are displayed to the console.
+
+This is basically encoding 101 - nothing tricky and nothing multi-step.
+
+If you're past the first stage of a puzzle, this probably won't help you,
+    at least not in a single run.
 """
 
 __author__ = "0rigen"
@@ -12,21 +17,21 @@ __web__ = "0rigen.net"
 __license__ = "GPL"
 __version__ = 3.0
 
-import sys
 import os
+import sys
 
-print '\n\033[95m' + 'DumbPuzzler v1.0' + '\033[0m'
-print '\033[92m' + 'by: @_0rigen' + '\033[0m'
-print '\nSelect an option to continue'
+print('\n\033[95m' + 'DumbPuzzler v1.0' + '\033[0m')
+print('\033[92m' + 'by: @_0rigen' + '\033[0m')
+print('\nSelect an option to continue')
 
 #######################
 # Menu for operations #
 #######################
 class Menu:
     def showmenu(self):
-        print "(" + '\033[1m\033[95m' + "S" + '\033[0m' + ")imple Encoding Checks"
-        print "(" + '\033[1m\033[95m' + "P" + '\033[0m' + ")unch 1o57 in the face"
-        print "(" + '\033[1m\033[95m' + "Q" + '\033[0m' + ")uit DumbPuzzler"
+        print("(" + '\033[1m\033[95m' + "S" + '\033[0m' + ")imple Encoding Checks")
+        print("(" + '\033[1m\033[95m' + "P" + '\033[0m' + ")unch 1o57 in the face")
+        print("(" + '\033[1m\033[95m' + "Q" + '\033[0m' + ")uit DumbPuzzler")
 
 
 #######################
@@ -35,9 +40,9 @@ class Menu:
 class Solver:
     def simple(self):
         try:
-            execfile("SimpleEncodingChecks.py")
+            exec (compile(open("SimpleEncodingChecks.py").read(), "SimpleEncodingChecks.py", 'exec'))
         except KeyboardInterrupt:
-            print 'Interrupted - See ya later!'
+            print('Interrupted - See ya later!')
             try:
                 sys.exit(0)
             except SystemExit:
@@ -48,14 +53,14 @@ class Solver:
 #######################
 class Chooser:
     def getchoice(self):
-        choice = raw_input(">>> ")
+        choice = input(">>> ")
         if choice in ('S', 's'):
-            print 'You chose Simple.'
+            print('You chose Simple.')
             s.simple()
         if choice in ('P', 'p'):
-            print 'You punched 1057!  I hope you feel a little better now...\n'
+            print('You punched 1057!  That was bad, and you should feel bad!\n')
         if choice in ('Q', 'q'):
-            print 'Good luck!'
+            print('Good luck!')
             sys.exit(0)
 
 # create instances of classes
--- src/SimpleEncodingChecks.py
+++ src/SimpleEncodingChecks.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env/ python
+#!/usr/bin/env python3
 """ Performs checks for the 'simple' ciphers
 
 This module is called when the user wants to perform (S)imple
@@ -11,20 +11,19 @@ __web__ = "0rigen.net"
 __license__ = "GPL"
 __version__ = 3.0
 
-import sys
-import string
-import binascii
 import base64
+import binascii
+import string
+import sys
 import time
-import encodings
 
 ###############
 # Resources   #
 ###############
 
-# Atbash translate key
+# Atbash translation key
 global atbash
-atbash = string.maketrans(
+atbash = str.maketrans(
     "ABCDEFGHIJKLMabcdefghijklmNOPQRSTUVWXYZnopqrstuvwxyz",
     "ZYXWVUTSRQPONzyxwvutsrqponMLKJIHGFEDCBAmlkjihgfedcba")
 
@@ -32,7 +31,8 @@ atbash = string.maketrans(
 global crypto_in
 
 # letters 2 numbers dictionary
-l2nd = { x+1: chr(ord('a')+x) for x in range(26) }
+l2nd = {x + 1: chr(ord('a') + x) for x in range(26)}
+
 
 #############################
 # bcolors for coloring text #
@@ -49,24 +49,44 @@ class TextColors:
     UNDERLINE = '\033[4m'
     ENDC = '\033[0m'
 
+
 #######################
 # Get input from user #
 #######################
 
-crypto_in = raw_input(TextColors.GREEN + 'Your new data to analyze --> ' + TextColors.ENDC)  # string of crypto
-input_length = raw_input(
-    TextColors.GREEN + '\nLength of Data Chunks (1 for text/ROTs/etc) --> ' + TextColors.ENDC)  # length of chunks
-print(
-    '\n' + TextColors.GREEN + "Original Input (chunk length " + input_length + "): " + TextColors.BOLD +
-                                                    crypto_in + TextColors.ENDC)
+crypto_in = input('Your new data to analyze --> ')  # string of crypto
+
+###########################################################################################################
+# Ok, the chunk thing can be a little confusing... so take a look at this:
+# A chunk is the length of a single value of enciphered/encoded text.
+#
+# For example: a string of numbers with no separation, "1634534734543", would be chunk size
+# '1', since every number, as far as you know, is to be evaluated individually.  The same is true of strings
+# of non-organized (!words) letters, "abcddefgh".
+#
+# If, on the other hand, you have a string of hex, "10 a1 16 3b c3 7d", the chunk size
+# would be '2', since the hex codes are each length 2 in the input string.
+#
+# Binary input is chunk size '8', since 1 = 00000001, 2 = 00000020, and so forth in binary.
+#
+# Thus, choose a chunk size that represents what you believe to represent how the data is
+# to be read as individual units.  Look for some form of separators.
+# If all else fails, just use '1' and see what comes out.
+##########################################################################################################
+input_length = input(
+    TextColors.GREEN + '\nLength of Data Chunks (1 for text/ROTs, 2 for hex, etc.) --> ' \
+    + TextColors.ENDC)  # length of chunks
 
 # Convert inputted chunk size to an integer
 try:
     chunk_size = int(input_length)
-    done=True
+    done = True  # I dont remember why this is here ^.^
+
 except ValueError:
-    print TextColors.RED + TextColors.BOLD + '\nChunk size must be an Integer!'
-    print "I'm just going to assume you meant " + TextColors.BLUE + TextColors.BOLD + "'1'\n" + TextColors.ENDC
+    print(TextColors.RED + TextColors.BOLD + '\nChunk size must be an Integer!')
+    print(
+        TextColors.GREEN + '[-] ' + "I'm just going to assume you meant " + \
+        TextColors.BLUE + TextColors.BOLD + "'1'\n" + TextColors.ENDC)
     chunk_size = 1
     time.sleep(2)
 
@@ -76,11 +96,15 @@ crypto_in = crypto_in.replace(" ", "")
 ######################
 # Chunk up the input #
 ######################
+print("""
+ -------- Processing Input ---------
+""")
+
 try:
     inarray = list(crypto_in)  # turn the input into an array of elements
 
     # Break the array up into chunks of size input_length
-    list_of_chunks = lambda lst, sz: [lst[i:i+sz] for i in range(0, len(lst), sz)]
+    list_of_chunks = lambda lst, sz: [lst[i:i + sz] for i in range(0, len(lst), sz)]
     chunked_array = list_of_chunks(inarray, chunk_size)
 
     # create strings out of the chunks
@@ -88,187 +112,251 @@ try:
     while i < chunked_array.__len__():
         chunked_array[i] = ''.join(chunked_array[i])
         i += 1
-    print TextColors.GREEN + 'Your chunked data: %s ' % chunked_array + TextColors.ENDC
+    print(TextColors.GREEN + '[-] ' + TextColors.ENDC + 'chunked data: %s ' % chunked_array \
+          + TextColors.ENDC)
 
 except ValueError:
-    print TextColors.RED + "Failed to parse your input string - shutting down" + TextColors.ENDC
+    print(
+        TextColors.RED + "[!] " + TextColors.ENDC + "Failed to parse your input string - shutting down" + TextColors.ENDC)
     sys.exit(0)
 
-print TextColors.GREEN + 'Input is ' + str(len(crypto_in)) + ' elements long, divided into %d chunks' % \
-                                                             chunked_array.__len__() + TextColors.ENDC
+print(TextColors.GREEN + '[-] ' + TextColors.ENDC + 'Data is ' + str(
+    len(crypto_in)) + ' elements long, divided into %d chunks' % \
+      chunked_array.__len__() + TextColors.ENDC)
+print("""
+ -------- Begin Checks ---------
+""")
+
+##########################
+# Check for a palindrome #
+##########################
+# Fun pythonic way to do this easily!
+if str(crypto_in) == str(crypto_in)[::-1]:
+    print(TextColors.GREEN + '[+] ' + "Looks like a palindrome to me" + TextColors.ENDC)
+else:
+    print(TextColors.RED + '[X] ' + "Not a palindrome" + TextColors.ENDC)
 
 ########################
 # Alpha --> ASCII Code #
 ########################
-print '\n' + TextColors.PURPLE + TextColors.BOLD + 'Transformed into ASCII Code Values: ' + TextColors.ENDC
 try:
+    output = ""
+
     for thing in chunked_array:
-        if ord(thing) > 176:
-            print '[Not ASCII]',
+        if ord(thing) > 176:  # #s > 176 are not ASCII
+            output += " [Non-ASCII]"
         else:
-            print ord(thing),
+            output = output + " " + str(ord(thing))
+
+    print(TextColors.GREEN + '\n[-] ' + "Alpha to ASCII Codes: " + TextColors.ENDC)
+    print(output)
+
 except:
     pass
-    print TextColors.RED+"Transforming into ASCII values failed."+TextColors.ENDC
+    print(TextColors.RED + '\n[X] ' + TextColors.ENDC + "Transforming into ASCII values failed." + TextColors.ENDC)
 
 ########################
 # ASCII Code --> Alpha #
 ########################
-print '\n' + TextColors.PURPLE + TextColors.BOLD + 'Transforming from ASCII codes to text: ' + TextColors.ENDC
 try:
+    fails = 0
+    output = ""
+
     for thing in chunked_array:
-        if int(thing) > 176:
-            print '[Not ASCII]',
+        if int(thing) > 176:  # #s greater than 176 are non-ascii
+            output += " [Non-Alpha]"
+            fails += 1
         else:
-            print str(unichr(int(thing))),
+            output = output + " " + str(chr(int(thing)))
+
+    print(TextColors.GREEN + '\n[-] ' + "ASCII Codes to Alpha: " + TextColors.ENDC)
+    print(output)
+
 except:
     pass
-    print TextColors.RED+"Transforming into ASCII values failed."+TextColors.ENDC
+    print(TextColors.RED + '\n[X] ' + TextColors.ENDC + "Transforming into alpha values failed." + TextColors.ENDC)
 
 ########################
 #  Numbers to Letters  #
 ########################
-print '\n\n' + TextColors.BOLD + TextColors.PURPLE + 'Transforming from numbers to alpha (1-26): ' + TextColors.ENDC
 try:
+    output = ""
+
     for code in chunked_array:
-        ltr = int(code.lstrip('0'))
+        ltr = int(code.lstrip('0'))  # strip any leading 0s, they're dumb and unnecessary
         if ltr > 26:
-            print TextColors.RED + "[Not a letter]" + TextColors.ENDC,
+            output += " [Non-Alpha]"
         else:
-            print(l2nd[ltr]),
+            output = output + " " + (l2nd[ltr])
+
+    print(TextColors.GREEN + '\n[-] ' + "Numbers to Letters: " + TextColors.ENDC)
+    print(output)
+
 except:
     pass
-    print TextColors.RED+"Transforming into text failed."+TextColors.ENDC
+    print(TextColors.RED + '\n[X] ' + TextColors.ENDC + "Transforming #s into text failed." + TextColors.ENDC)
 
 ########################
 #  Letters to Numbers  #
 ########################
-print '\n\n' + TextColors.BOLD + TextColors.PURPLE + 'Transforming letters into numeric alphabet (a=1, etc): ' + TextColors.ENDC
 try:
+    output = ""
+
     for elem in chunked_array:
-        value = ord(elem) - 96
-        print value,
+        value = ord(elem) - 96  # The actual numerical value is ascii ord - 96
+        output = output + " " + str(value)
+
+    print(TextColors.GREEN + '\n[-] ' + "Letters to Numbers: " + TextColors.ENDC)
+    print(output)
+
 except:
     pass
-    print TextColors.RED+"Couldn't turn letters into numbers"+TextColors.ENDC
+    print(TextColors.RED + '\n[X] ' + TextColors.ENDC + "Couldn't turn letters into numbers" + TextColors.ENDC)
 
 ########################
 # CipherTxt --> Binary #
 ########################
-print '\n\n' + TextColors.BOLD + TextColors.PURPLE + 'Transformed into binary Values: ' + TextColors.ENDC
 try:
+    output = ""
+
     # This transform fails if it encounters a 'b' in the input.  That is handled as a special case
     for item in chunked_array:
         if item != 'b':
-            print bin(int(binascii.hexlify(item), 16)),
+            output = output + " " + str(bin(int(binascii.hexlify(item), 16)))
         elif item == 'b':
-            print '0b01100010'
+            output += ' 0b01100010'
+
+    print(TextColors.GREEN + '\n[-] ' + "To Binary: " + TextColors.ENDC)
+    print(output)
+
 except:
     pass
-    print TextColors.RED+"Transforming into binary failed."+TextColors.ENDC
+    print(TextColors.RED + '\n[X] ' + TextColors.ENDC + "Transforming into binary failed." + TextColors.ENDC)
 
 ########################
 #  Binary --> Integer  #
 ########################
-print '\n\n' + TextColors.BOLD + TextColors.PURPLE + 'Transforming from binary into Integers: ' + TextColors.ENDC
 try:
+    output = ""
+
     for item in chunked_array:
-        print int(item, 2),  # puts the item into integer form, from base 2 :D Python so ez!
+        output = output + " " + str(int(item, 2))
+
+    print(TextColors.GREEN + '\n[-] ' + "Binary to Integer: " + TextColors.ENDC)
+    print(output)
+
 except:
-    print TextColors.RED+"\nTransforming binary to Integer failed."+TextColors.ENDC
+    print(TextColors.RED + '\n[X] ' + TextColors.ENDC + "Transforming binary to Integer failed." + TextColors.ENDC)
 
 ########################
 # Binary --> ASCII Ltr #
 ########################
-print '\n\n' + TextColors.PURPLE + TextColors.BOLD + 'Transformed from binary to text ' + TextColors.ENDC
 try:
+    output = ""
+
     for thing in chunked_array:
-            n = int(thing, 2)
-            print binascii.unhexlify('%x' % n),
+        n = int(thing, 2)
+        output = output + " " + str(binascii.unhexlify('%x' % n))
+
+    print(TextColors.GREEN + '\n[-] ' + "Binary to ASCII Letters: " + TextColors.ENDC)
+    print(output)
 
 except:
     pass
-    print TextColors.RED+"\nTransforming from binary to text failed."+TextColors.ENDC
+    print(TextColors.RED + '\n[X] ' + TextColors.ENDC + "Transforming from binary to text failed." + TextColors.ENDC)
 
 ########################
 #     Hex --> Text     #
 ########################
-print '\n\n' + TextColors.BOLD + TextColors.PURPLE + 'Transformed out of Hex into ASCII (ignoring chunk' \
-                                                     ' size, sry not sry):' + TextColors.ENDC
 try:
-    out = ''.join(chr(int(crypto_in[i:i+2], 16)) for i in range(0, len(crypto_in), 2))
-    print out
+    out = ''.join(chr(int(crypto_in[i:i + 2], 16)) for i in range(0, len(crypto_in), 2))
+
+    print(TextColors.GREEN + '\n[-] ' + "Hex to Text: " + TextColors.ENDC)
+    print(out)
 except:
-    print TextColors.RED+"Transforming into hex failed."+TextColors.ENDC
+    print(TextColors.RED + '\n[X] ' + TextColors.ENDC + "Transforming into hex failed." + TextColors.ENDC)
 
 ########################
 #        ROT           #
 ########################
-print '\n' + TextColors.BOLD + TextColors.PURPLE + 'ROT: ' + TextColors.ENDC
+""" This will go ahead and run through every potential rotation, JFF """
+
 message = crypto_in
 alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+print("\n++++ ROT ++++")
+
 for key in range(len(alphabet)):
 
     rot_out = ''
 
-    for symbol in message:       # rot each letter in the input ciphertext
+    for symbol in message:  # rot each letter in the input ciphertext
         if symbol.capitalize() in alphabet:
             num = alphabet.find(symbol.capitalize())
-            num = num + key
+            num += key
 
-            if num > 25:            # correct wrap-around
-                num = num - 26
+            if num > 25:  # correct wrap-around
+                num -= 26
 
-            rot_out = rot_out + alphabet[num]  # add to output string
+            rot_out += alphabet[num]  # add to output string
 
         else:
             rot_out = rot_out + symbol
 
-    print('Key #%s: %s' % (key, rot_out))
+    print('ROT%s: %s' % (key, rot_out))
 
 ########################
 #        ATBASH        #
 ########################
-print '\n' + TextColors.BOLD + TextColors.PURPLE + 'ATBASH ' + TextColors.ENDC
 try:
-    print string.translate(crypto_in, atbash)
+    output = str(string.translate(crypto_in, atbash))  # Use the dictionary defined at beginning
+    print(TextColors.GREEN + '\n[-] ' + "ATBASH: " + TextColors.ENDC)
+    print(output)
 except:
     pass
-    print TextColors.RED+"ATBASH failed."+TextColors.ENDC
+    print(TextColors.RED + '\n[X] ' + TextColors.ENDC + "ATBASH failed." + TextColors.ENDC)
 
 ########################
 #    Hex --> Base10    #
 ########################
-print '\n' + TextColors.BOLD + TextColors.PURPLE + 'Transformed hex to base10 as singles: ' + TextColors.ENDC
 if chunk_size == 2:
     try:
+        output = ""
         for h in chunked_array:
-            print int(h, 16),
+            output = output + " " + str(int(h, 16))
+
+        print(TextColors.GREEN + '\n[-] ' + "Hex to Base10: " + TextColors.ENDC)
+        print(output)
+
     except:
         pass
-        print '\n' + TextColors.RED+"Single hex to base10 failed"+TextColors.ENDC
+        print('\n' + TextColors.RED + "\n[X] Hex to base10 failed." + TextColors.ENDC)
+
 else:
-    print TextColors.RED+"It can't be hex2b10 since the chunk size isn't 2..."+TextColors.ENDC
+    print(
+        TextColors.RED + '\n[X] ' + TextColors.ENDC + "It can't be hex2b10 since the chunk size isn't 2..." + TextColors.ENDC)
 
 ########################
 #    Base64 Decoding   #
 ########################
-print '\n' + TextColors.BOLD + TextColors.PURPLE + 'Attempting to decode as base64: ' + TextColors.ENDC
 try:
     encoded = crypto_in
-    print base64.b64decode(encoded)
+    output = str(base64.b64decode(encoded))  # Well, that was easy
+    print(TextColors.GREEN + '\n[-] ' + "Base64 Decoding: " + TextColors.ENDC)
+    print(output)
+
 except TypeError:
     pass
-    print TextColors.RED+'TypeError Decoding Base64: Non-alphabet characters or improper padding of input' \
-                         'string, most likely'+TextColors.ENDC
+    print(TextColors.RED + '\n[X] TypeError Decoding Base64: Non-alphabet characters or improper padding of input' \
+                           'string, most likely' + TextColors.ENDC)
 except:
     pass
-    print '\n' + TextColors.RED+"Base64 failed"+TextColors.ENDC
+    print(TextColors.RED + '\n[X] ' + TextColors.ENDC + "Base64 failed" + TextColors.ENDC)
 
 ########################
 # Print final warnings #
 ########################
-print TextColors.BOLD + TextColors.YELLOW + "\n\nSimple Checks Complete!  Hope you found what you were " \
-                                            "looking for!\n" + TextColors.ENDC
-print TextColors.PURPLE + "Remember, if it's a sequence of numbers, it would be worth searching on" \
-                          "http://oeis.org for a named sequence!" + TextColors.ENDC + "\n"
+print(TextColors.BOLD + TextColors.YELLOW + "\n\nSimple Checks Complete!  Hope you found what you were " \
+                                            "looking for!\n" + TextColors.ENDC)
+print(TextColors.PURPLE + "Remember, if it's a sequence of numbers, it would be worth searching on" \
+                          "http://oeis.org for a named sequence!" + TextColors.ENDC + "\n")
